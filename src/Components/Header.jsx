7be20ab55ba9ada42e65cfbebcabd5656580bcb4
ByeolNabi <narwhal_blast@naver.com>
2025-03-29 21:56:58 +0900

    아이콘 추가 및 경로수정

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,12 @@
-
-
 function Header() {
     return (
         <header>
             <div className="contents">
                 <div className="left">
-                    <img src="public/favicon-32x32.png" style={{ width: '28px', height: '28px' }} />
+                    <img src="/under-phase.svg" style={{ width: '28px', height: '28px' }} />
                     <h1>&nbsp;&nbsp;| 사무실 현황</h1></div>
                 <div className="right">
-                    <img className="ham-menu" src="public/hamburger.svg" />
+                    <img className="ham-menu" src="/hamburger.svg" />
                 </div>
             </div>
         </header>
--- src/Components/PersonCard.jsx
+++ src/Components/PersonCard.jsx
@@ -2,14 +2,16 @@ function PersonCard(props) {
     const { personInfo, button, onButtonClick } = props;
     const format = { hour: 'numeric', minute: 'numeric', hour12: true }; // 12시간 형식
     const timeString = personInfo.date.toLocaleTimeString('en-US', format);
+
+    const img_path = button == '+' ? '/plus-button.svg' : '/minus-button.svg'
     return (
         <div className="person-card" draggable>
             <div className="card-top">
-                <button onClick={() => onButtonClick(personInfo)}>{button}</button>
+                <img onClick={() => onButtonClick(personInfo)} src={img_path}/>
             </div>
             <div className="card-mid">
                 <div>{personInfo.userName}</div>
-                <div style={{ fontSize: "15px" , textAlign:"end", width:"85%"}}>{timeString}</div>
+                <div style={{ fontSize: "15px", textAlign: "end", width: "85%" }}>{timeString}</div>
             </div>
         </div>
     );
